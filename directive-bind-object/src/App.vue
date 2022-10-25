1098ed611b401917280f7ab9f96efb967096b885
shoisaitoh <saitoh@ir-alt.co.jp>
2022-10-25 12:04:11 +0900

    upto P62

---
+++ directive-bind-object/src/App.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const imgAttributes = ref({
+  src: "/images/logo.svg",
+  alt: "Vueのロゴ",
+  width: 75,
+  height: 75
+});
+</script>
+
+<template>
+  <p><img v-bind="imgAttributes"></p>
+  <p><img v-bind="imgAttributes" title="ロゴです！"></p>
+  <p><img v-bind="imgAttributes" alt="ロゴです！"></p>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
