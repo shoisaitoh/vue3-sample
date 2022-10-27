797c7fe43dc9dc0c46d084e3d04b48841518cf22
shoisaitoh <saitoh@ir-alt.co.jp>
2022-10-27 18:40:57 +0900

    upto P128

---
+++ directive-loop-range/src/App.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <ul>
+    <li
+      v-for="r in 5"
+      v-bind:key="r">
+      半径{{r}}の円の円周: {{2*r*3.14}}
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
